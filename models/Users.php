eb0c63a81da9b53aee97f31256d1ddf11bf6b3f9
Charush <charush194@gmail.com>
2017-08-30 01:56:42 +0530

    debug

--- models/Users.php
+++ models/Users.php
@@ -742,9 +742,9 @@ public function readInfo(){
         $mail->IsSMTP(); // enable SMTP
         $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
         $mail->SMTPAuth = true; // authentication enabled
-        $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+        $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
         $mail->Host = "smtp.gmail.com";
-        $mail->Port = 465; // or 587
+        $mail->Port = 587; // or 587
         $mail->IsHTML(true);
         $mail->Username = "charush@accubits.com";
         $mail->Password = "charush*523";
