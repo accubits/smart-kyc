d5186766dcdfb1286ac2bb04f042a23e004560f6
Charush <charush194@gmail.com>
2017-08-30 02:22:26 +0530

    debug

--- models/Users.php
+++ models/Users.php
@@ -740,7 +740,7 @@ public function readInfo(){
     public function sendMail($to,$sub,$content) {
         $mail = new  PHPMailer(); // create a new object
         $mail->IsSMTP(); // enable SMTP
-        $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+        $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
         $mail->SMTPAuth = true; // authentication enabled
         $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
         $mail->Host = "smtp.gmail.com";
