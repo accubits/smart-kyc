cd2e1a7a47ba2ab4fa95c2967f21a434d5238590
Charush <charush194@gmail.com>
2017-09-10 00:34:01 +0530

    added email verification

--- config/config.php
+++ config/config.php
@@ -101,6 +101,7 @@ class dbconfig
         return "<!doctype html>
 <html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\">
 <head>
+    <base href=\"http://52.220.41.10/\"/>
     <meta charset=\"utf-8\">
     <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
