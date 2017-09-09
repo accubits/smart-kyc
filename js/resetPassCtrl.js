d2779f94b3267c9da728d97bfa4614d4e2b8fe09
athul <athul@accubits.com>
2017-09-09 22:31:36 +0530

    corrections as per new list

--- js/resetPassCtrl.js
+++ js/resetPassCtrl.js
@@ -1,3 +1,4 @@
+var crypbrokersApp = angular.module('crypbrokersApp', []);
 crypbrokersApp.controller('verifyCntl', function ($scope,$http) {
     $http.defaults.headers.post["Accept"] = "";
     $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
